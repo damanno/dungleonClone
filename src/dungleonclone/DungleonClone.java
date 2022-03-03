61abe12d1b566c5ef5d28a88097d0baa9c982af6
damanno <damanno@coderp.sp.gov.br>
2022-03-03 14:43:05 -0300

    Dungeon game logic all necessary methods

--- src/dungleonclone/DungleonClone.java
+++ src/dungleonclone/DungleonClone.java
@@ -5,28 +5,61 @@
  */
 package dungleonclone;
 
+import java.util.Scanner;
+
 /**
  *
  * @author xeldao
  */
 public class DungleonClone {
 
-    public static String[] cards = {"H1", "H2", "H3", "H4", "T1", "T2", "T3", "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9"};
+    public static String[] cards = {"H1", "H2", "H3", "T1", "T2", "E1", "E2", "E3", "E4", "E5"};
     public static String[] solution = {"", "", "", "", ""};
+    public static String[][] answers;
     public static int mistakes = 6;
     public static int attempts = 6;
 
     public static void main(String[] args) {
         // TODO code application logic here
         init();
-    }
+        
+        
+        /*
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Entre com a  carta");
 
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("Username is: " + userName); 
+        */
+    }
+    
+    //Show options
+    public static void printOptions() {
+    
+    }
+    
+    public static void gameOver() {
+    
+    }
+    
+    public static void rules() {
+    
+    }
+    
+    public static void score() {
+    
+    }
+    
     public static void input() {
-
+        
     }
 
+    public static void check() {
+        
+    }
+    
     public static void tip() {
-
+        
     }
 
     public static void init() {
@@ -36,8 +69,8 @@ public class DungleonClone {
         while (enemyPlacer == heroPlacer) {
             enemyPlacer = (int) (Math.random() * (solution.length - 1));
         }
-        solution[heroPlacer] = cards[(int) (Math.random() * (4 - 1))];
-        solution[enemyPlacer] = cards[(int) (Math.random() * (cards.length - 8)+8)];
+        solution[heroPlacer] = cards[(int) (Math.random() * (3 - 1))];
+        solution[enemyPlacer] = cards[(int) (Math.random() * (cards.length - 5)+5)];
 
         //rnd void
         for (int i = 0; i < 5; i++) {
@@ -48,9 +81,6 @@ public class DungleonClone {
         for (int i = 0; i < 5; i++) {
             System.out.print(solution[i] +" ");
         }
-        
-        
-        
     }
 
 }
